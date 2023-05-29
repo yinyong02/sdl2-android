a546676441034f6bff1997cf2c855352cb6ddeaa
yinyong <2019151183@qq.com>
2023-05-29 14:23:10 +0800

    init

---
+++ README.md
@@ -0,0 +1,21 @@
+此仓库是为了提供SDL2 for android版本的编译库
+
+
+## 使用方法
+
+
+``sh
+git clone
+``
+
+## 编译方法
+
+clean
+```sh
+/Volumes/WDSN570/Library/android_sdk/ndk/25.1.8937393/ndk-build clean
+```
+
+build
+```sh
+/Volumes/WDSN570/Library/android_sdk/ndk/25.1.8937393/ndk-build
+```
---
+++ jni/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Description: 
+
+# Usage: 
+
+# Author: 
+
+# Date:
+
+# /Volumes/WDSN570/Library/android_sdk/ndk/25.1.8937393/ndk-build
+
+# /Volumes/WDSN570/Library/android_sdk/ndk/25.1.8937393/ndk-build clean
+
+/Volumes/WDSN570/Library/android_sdk/ndk/25.1.8937393/ndk-build clean
+/Volumes/WDSN570/Library/android_sdk/ndk/25.1.8937393/ndk-build
